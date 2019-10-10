e333ed3dba21e371da6b1c5cd4c11659c7d0981d
Andrei Zubarev <45924190+AndreiZubarev1985@users.noreply.github.com>
2019-10-10 20:16:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function multiply(first, second) {
   // your solution
+  const product = Array(first.length+second.length).fill(0);
+        for (let i = first.length; i--; null) {
+            let carry = 0;
+            for (let j = second.length; j--; null) {
+                product[1+i+j] += carry + first[i]*second[j];
+                carry = Math.floor(product[1+i+j] / 10);
+                product[1+i+j] = product[1+i+j] % 10;
+            }
+            product[i] += carry;
+            console.log(product);
+        }
+        if(product[0] === 0) {
+            let result = product.shift();
+            return product.join('');
+        } else {
+            return product.join('');
+        }
+        return product;
+    }
 }
